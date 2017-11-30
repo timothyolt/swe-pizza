3ff80de961797ca11f9e509e7cca6d1362226cb1
Andrew Mattie <andrewmattie@gmail.com>
2017-11-30 03:21:12 -0500

    Add link to manage site

--- web-pizza/src/app/app.component.ts
+++ web-pizza/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { log } from 'util';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +10,20 @@ import { AngularFireAuth } from 'angularfire2/auth';
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
+  userInfo: Observable<UserInfo>;
   isLoggedIn = false;
+  isAdmin = false;
 
-  constructor(private auth: AngularFireAuth) {
+  constructor(private auth: AngularFireAuth, private db: AngularFireDatabase) {
     this.auth.auth.onAuthStateChanged(user => {
       this.isLoggedIn = user && !user.isAnonymous;
+      this.userInfo = this.db.object(`/users/${user.uid}`).valueChanges();
+      this.userInfo.subscribe(object => this.isAdmin = object.isAdmin);
     });
   }
+}
+
+class UserInfo {
+  activeOrder: string;
+  isAdmin: boolean;
 }
--- web-pizza/src/app/toppings-list/toppings-list.component.ts
+++ web-pizza/src/app/toppings-list/toppings-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import { ItemType } from '../../models/item-type';
 
@@ -9,7 +9,7 @@ import { ItemType } from '../../models/item-type';
   styleUrls: ['./toppings-list.component.css']
 })
 export class ToppingsListComponent implements OnInit {
-  itemTypes: Observable<ItemType[]>;  
+  itemTypes: Observable<ItemType[]>;
 
   constructor(private db: AngularFireDatabase) { }
 
