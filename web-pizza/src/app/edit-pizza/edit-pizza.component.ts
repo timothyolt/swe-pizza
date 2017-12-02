294396e74a7c2e3dab5e67d9884d43141366d97d
Andrew Mattie <andrewmattie@gmail.com>
2017-12-02 15:22:14 -0500

    WIP setup UI

--- web-pizza/src/app/edit-pizza/edit-pizza.component.ts
+++ web-pizza/src/app/edit-pizza/edit-pizza.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Pizza } from '../../models/pizza';
+import { AngularFireDatabase, AngularFireObject, SnapshotAction } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-edit-pizza',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-pizza.component.css']
 })
 export class EditPizzaComponent implements OnInit {
+  pizzaRef: AngularFireObject<Pizza>;
+  @Input() pizza = new Pizza();
+  @Input() itemCatSnapshots: Observable<SnapshotAction[]>;
 
-  constructor() { }
+  nameEditable = false;
+  pizzaName: string;
+
+  constructor(private db: AngularFireDatabase) {
+  }
 
   ngOnInit() {
+    this.itemCatSnapshots = this.db.list('/itemCat').snapshotChanges().shareReplay(1);
+    this.itemCatSnapshots.subscribe(console.log);
   }
 
 }
