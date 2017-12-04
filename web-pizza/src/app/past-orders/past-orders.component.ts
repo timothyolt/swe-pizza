04da1ccf9432624885b2b856359a04b6c50f84e0
Timothy Oltjenbruns <timothyolt@gmail.com>
2017-12-03 19:58:52 -0500

    receipts and past orders

--- web-pizza/src/app/past-orders/past-orders.component.ts
+++ web-pizza/src/app/past-orders/past-orders.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AngularFireDatabase, SnapshotAction } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-past-orders',
   templateUrl: './past-orders.component.html',
   styleUrls: ['./past-orders.component.css']
 })
-export class PastOrdersComponent implements OnInit {
+export class PastOrdersComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  orders: Observable<SnapshotAction[]>;
+  orderDetails: {[key: string]: Observable<String>} = {};
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase, private auth: AngularFireAuth) { }
 
   ngOnInit() {
+    this.subscription = this.auth.authState.subscribe(user => {
+      if (user) {
+        this.orders = this.db.list('users/' + user.uid + '/orders').snapshotChanges();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  orderName(key: string): Observable<String> {
+    if (!this.orderDetails[key]) {
+      this.orderDetails[key] = this.db.object('orders/' + key + '/createdAt').valueChanges();
+    }
+    return this.orderDetails[key];
   }
 
 }
